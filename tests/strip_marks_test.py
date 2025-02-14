bbe01b9d366e77a34a6207a9377d5684ec9e6a33
Jake Ireland <jakewilliami@icloud.com>
2025-02-15 09:33:36 +1300

    Instantiate empty project with test cases

---
+++ tests/strip_marks_test.py
@@ -0,0 +1,20 @@
+from typing import Iterable
+
+import pytest
+
+from src import strip_marks
+
+
+@pytest.fixture
+def unicode_stripped_pairs() -> Iterable[tuple[str, str]]:
+    def genpairs() -> Iterable[tuple[str, str]]:
+        yield "kaderdenkesişenyollarinhikayesi.xyz", "kaderdenkesisenyollarinhikayesi.xyz"
+        yield "şéàşöñ", "season"
+        yield "かな漢字", "かな漢字"  # These are identical
+
+    yield from genpairs()
+
+
+def test_strip_marks(unicode_stripped_pairs: Iterable[tuple[str, str]]):
+    for (s1, s2) in unicode_stripped_pairs:
+        assert strip_marks.strip_marks(s1) == s2
